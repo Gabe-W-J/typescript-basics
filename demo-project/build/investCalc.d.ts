a2ee8089302dcf93540e8a307cf7fadbd41c9784
Gabe.W.J <gabriel.winston.jackson@gmail.com>
2025-10-01 16:54:21 +0200

    Added the demo-project section

---
+++ demo-project/build/investCalc.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=investCalc.d.ts.map
---
+++ demo-project/investCalc.ts
@@ -0,0 +1,89 @@
+//* Note: I actually coded this on my own, without following the section of the course. So things may not be 1:1 to what the course does.
+
+type Investment = {
+  investmentName?: string;
+  startingInvestment: number;
+  extraPerYear: number;
+  highExpectedReturnRate: number;
+  lowExpectedReturnRate: number;
+  averageReturnRate: number;
+};
+
+type Result = {
+  investmentName?: string | undefined;
+  yearOfInvestment: number;
+  startOfYearPortfolio: number;
+  endOfYearPortfolio: number;
+  profit: number;
+  profitPercentage: number;
+};
+
+function calculator(data: Investment, duration: number): Result[] {
+  let results: Result[] = [];
+  let currentPortfolio = data.startingInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const investmentName = data.investmentName;
+    const yearOfInvestment = i + 1;
+    const startOfYearPortfolio = currentPortfolio + data.extraPerYear;
+    const profitPercentage = Math.floor(
+      (Math.random() *
+        (data.highExpectedReturnRate - data.lowExpectedReturnRate + 1) +
+        data.lowExpectedReturnRate +
+        data.averageReturnRate) /
+        2
+    );
+
+    const profit = Number((Math.floor(((startOfYearPortfolio * profitPercentage) / 100) * 100) / 100).toFixed(2)); // This ends up being a float without Number(.toFixed()) ._.
+    
+    const endOfYearPortfolio = Number((startOfYearPortfolio + profit).toFixed(2)); //! For some reason, unless I Number(.toFixed()) this too, it'll be a float >:(
+    currentPortfolio = endOfYearPortfolio
+
+    results.push({
+      investmentName: investmentName,
+      yearOfInvestment: yearOfInvestment,
+      startOfYearPortfolio: startOfYearPortfolio,
+      endOfYearPortfolio: endOfYearPortfolio,
+      profit: profit,
+      profitPercentage: profitPercentage,
+    });
+  }
+
+  return results;
+}
+
+function printResults(results: Result[]) {
+    let totalProfit: number = 0 //! Here too LMAO
+    console.log("-----------------------------------------------------------")
+    console.log(`Investment Calculations for ${results[1]?.investmentName}`)
+    for (let i in results){
+        const result = results[i]
+        totalProfit += result?.profit ?? 0
+        console.log("-----------------------------------------------------------")
+        console.log("")
+        console.log(`Year: ${result?.yearOfInvestment}`)
+        console.log(`Starting Value: ${result?.startOfYearPortfolio}`)
+        console.log(`End of Year Value: ${result?.endOfYearPortfolio}`)
+        console.log(`Profit: € ${result?.profit}`)
+        console.log(`Profit Percentage: ${result?.profitPercentage}%`)
+        console.log("")
+    }
+    console.log("-----------------------------------------------------------")
+    console.log(`Total Profit: € ${Number(totalProfit.toFixed(2))}`)
+    console.log("-----------------------------------------------------------")
+    console.log("")
+    console.log("End of line.")
+}
+
+const investment: Investment = {
+  investmentName: "S&P500 // HZ4F",
+  startingInvestment: 432.71, // In euros (€)
+  extraPerYear: 0,
+  highExpectedReturnRate: 12, // Note that these are percentages
+  lowExpectedReturnRate: -1.44,
+  averageReturnRate: 9.96,
+};
+
+printResults(calculator(investment, 10));
+
+//* Note 2: After watching the section of the course, I can confirm it's very not 1:1 but definitely contains all of the same concepts
